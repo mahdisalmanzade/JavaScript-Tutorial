e4615f46cdf4b177c49546247541bf656570e6bd
Mahdi Salmanizadehgan <mahdisalmanizadehgan@Mahdis-MacBook-Pro.local>
2022-09-02 15:39:56 +0430

    latest updates

--- DOMs/events.js
+++ DOMs/events.js
@@ -2,8 +2,17 @@ const form = document.querySelector('.main-form');
 const btn = document.querySelector('#submit');
 
 form.addEventListener('submit', function (event) {
-  let username = document.querySelector('#username').value;
-  let email = document.querySelector('#email').value;
-  event.preventDefault();
-  console.log(`Your username is ${username} and your email is ${email}`);
+  let username = document.querySelector('#username');
+  let email = document.querySelector('#email');
+  if (username.value && email.value) {
+    event.preventDefault();
+    console.log(
+      `Your username is ${username.value} and your email is ${email.value}`
+    );
+    username.value = '';
+    email.value = '';
+  } else {
+    event.preventDefault();
+    console.log('Insert something!');
+  }
 });
